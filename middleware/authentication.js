3795ad69f3793315ce7cd837b90cd88f8a2dce51
Yelena <88336329+DYA13@users.noreply.github.com>
2023-05-05 10:45:06 +0200

    modified:   middleware/authentication.js

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,14 +1,26 @@
 const User = require('../models/User')
 const jwt = require('jsonwebtoken')
 const {UnauthenticatedError} = require('../errors')
-const auth = (req, res, next) => {
+
+
+const auth = async (req, res, next) => {
     //check header
-    const authHeader = req.headers.authorization
+    const authHeader =  req.headers.authorization
 if(!authHeader || !authHeader.startsWith('Bearer')){
-    
+throw new UnauthenticatedError('Authenticated invalid')
+}
+const token = authHeader.split('')[1]
+try{
+const payload = jwt.verify(token, process.env.JWT_SECRET)
+//attach the user to the job routes
+req.user =  { userId:payload.userId, name:payload.name }
+next()
+
+} catch (error){
+throw new UnauthenticatedError('Authenticated invalid')
+}
 }
 
-}
-
+module.exports = auth
 
 
--- models/User.js
+++ models/User.js
@@ -34,8 +34,8 @@ UserSchema.methods.createJWT = function () {
 return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET,{
     expiresIn: process.env.JWT_LIFETIME}) 
 }
-UserSchema.methods.comparePassword = async function (canditatePassword) {
-    const isMatch = await bcrypt.compare(canditatePassword, this.password)
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password)
     return isMatch
 }
 
