14b0536c215e1eb855a263ebacf33c549c1a2be9
Yelena <88336329+DYA13@users.noreply.github.com>
2023-05-05 14:28:31 +0200

    edited controllers/auth.js,jobs.js

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -4,10 +4,9 @@ const {BadRequestError, NotFoundError} = require('../errors')
 
 
 const getAllJobs = async (req, res) => {
-const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
-res.status(StatusCodes.OK).json({jobs, count: jobs.length})
-
-}
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
+  }
 
 const getJob = async( req, res) => {
     res.send('get job')
