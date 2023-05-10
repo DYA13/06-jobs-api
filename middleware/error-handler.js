c4151d4e3e19706cb842e906573a7adf35005b83
Yelena <88336329+DYA13@users.noreply.github.com>
2023-05-10 10:58:07 +0200

    new file:   .node-version,modified:   app.js,middleware/error-handler.js

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -12,6 +12,7 @@ msg:err.message || 'Something went wrong try again later'
   // }
 
 if (err.name === 'ValidationError'){
+
   customError.msg = Object.values(err.errors)
   .map((item)=> item.message)
   .join(',')
@@ -21,6 +22,10 @@ if (err.name === 'ValidationError'){
  if(err.code && err.code === 11000){
   customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
   customError.statusCode = 400
+ }
+ if(err.name === 'CastError'){
+  customError.msg = `No item found with id: ${err.value}`
+  customError.statusCode = 404
  }
   //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
   return res.status(customError.statusCode).json({ msg:customError.msg })
